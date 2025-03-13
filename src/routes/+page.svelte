99923a3ace2990dc4cb2e68cf4a10c96e6e101f9
Stefan Schörkmeier <S.Schoerkmeier@student.tugraz.at>
2025-03-13 12:02:31 +0100

    After min 40

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,39 +2,4 @@
 
     import Header from "./Header.svelte";
 
-    // let name = "Bernd"
-    let name = $state("Stefan");
-
-
-    let status: "OPEN" | "CLOSED" = $state("OPEN"); // OPEN | CLOSED is the type.
-    // function toggle() {
-    //     status = status === "OPEN" ? "CLOSED" : "OPEN";  // We update the value directly, there is no state.
-    // } 
-    function onclick() {
-        status = status === "OPEN" ? "CLOSED" : "OPEN";  // We update the value directly, there is no state.
-    }
-
-
-    // Derived state (gets its value from other values eg array)
-    let full_name = $derived(name + " " + "Fuckyeahoida")
-
 </script>
-
-<!-- <Header name="Stefan" /> -->
-<!-- <Header name={name} /> Same as: -->
- <Header {name} />  <!-- Really cool uwu , bellow as well xD -->
- <input type="text" bind:value={name} />
-
-
- <p>The store is now {status}</p>
- <!-- <button onclick={toggle}>Toggle Status</button> -->
- <!-- <button {onclick}>Toggle Status</button> -->
- <button onclick={() => {
-    status = status === "OPEN" ? "CLOSED" : "OPEN";
- }}>Toggle Status</button>
-
-
- <h2> {full_name} </h2>
-
-
- <Header {name} fake_name="FHA" />
--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,19 +1,8 @@
 <script lang="ts">
-    let { 
-        name, 
-        fake_name = "blubl" 
-    }: {
-        name: string; // To define types we use this notation.
-        fake_name?: string | null; // ? is for optional variables.
-    } = $props();  // $ are Runes :3 eg $state etc 
+
 </script>
 
-<h1> Hello from Header to {name} </h1>
-<!-- <h1> Hello from Header to {name === "Stefan"} </h1> -->
-<h2>{name.replaceAll('n', 'X')}</h2>
-
-
-{fake_name}
+<h1> Hello from Header</h1>
 
 
 
